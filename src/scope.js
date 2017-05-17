7c5b18897075bb69a35b069f47c82a86c18c18fa
bo kwon <boyoung@whitesky.com>
2017-05-16 17:26:52 -0700

    add more test cases in scope_spec.js

--- src/scope.js
+++ src/scope.js
@@ -2,24 +2,29 @@
 var _ = require('lodash');
 function Scope() {
 	this.$$watchers = [];
-}
+};
+function initWatchVal() { };
 
 Scope.prototype.$watch = function(watchFn, listenerFn){
 	var watcher = {
 		watchFn: watchFn,
-		listenerFn: listenerFn
+		listenerFn: listenerFn || function(){ },
+		last: initWatchVal
 	};
 	this.$$watchers.push(watcher);
 };
 
 Scope.prototype.$digest = function() {
 	var self = this;
-	
+	var newValue, oldValue;
 	_.forEach(this.$$watchers, function(watcher) {
-		watcher.watchFn(self);
-		watcher.listenerFn();
+		newValue = watcher.watchFn(self);
+		oldValue = watcher.last;
+		if (newValue !== oldValue) {
+			watcher.last = newValue;
+			watcher.listenerFn(newValue, (oldValue === initWatchVal ? newValue: oldValue), self);
+		}
 	});
 };
-
 
 module.exports = Scope;
