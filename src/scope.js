03e75a319712c2cfa15752e738c84a9d4ce6179f
bo kwon <boyoung@whitesky.com>
2017-05-19 17:01:46 -0700

    add to handle exceptions and destroying a watch

--- src/scope.js
+++ src/scope.js
@@ -7,29 +7,43 @@ function Scope() {
 function initWatchVal() { };
 
 Scope.prototype.$watch = function(watchFn, listenerFn, valueEq){
+	var self = this;
 	var watcher = {
 		watchFn: watchFn,
 		listenerFn: listenerFn || function(){ },
-		valueEq: valueEq,
-		last: !!initWatchVal
+		valueEq: !!valueEq,
+		last: initWatchVal
 	};
-	this.$$watchers.push(watcher);
+	this.$$watchers.unshift(watcher);
 	this.$$lastDirtyWatch = null;
+	return function() {
+		var index = self.$$watchers.indexOf(watcher);
+		if (index >= 0) {
+			self.$$watchers.splice(index, 1);	
+			self.$$lastDirtyWatch = null;
+		}
+	};
 };
 
 Scope.prototype.$$digestOnce = function() {
 	var self = this;
 	var newValue, oldValue, dirty;
-	_.forEach(this.$$watchers, function(watcher) {
-		newValue = watcher.watchFn(self);
-		oldValue = watcher.last;
-		if (newValue !== oldValue) {
-			self.$$lastDirtyWatch = watcher;
-			watcher.last = newValue;
-			watcher.listenerFn(newValue, (oldValue === initWatchVal ? newValue: oldValue), self);
-			dirty = true;
-		} else if (self.$$lastDirtyWatch === watcher) {
-			return false;
+	_.forEachRight(this.$$watchers, function(watcher) {
+		try {
+			if (watcher) {
+				newValue = watcher.watchFn(self);
+				oldValue = watcher.last;
+				if (!self.$$areEqual(newValue, oldValue, watcher.valueEq)) {
+					self.$$lastDirtyWatch = watcher;
+					watcher.last = (watcher.valueEq ? _.cloneDeep(newValue) : newValue);
+					watcher.listenerFn(newValue, (oldValue === initWatchVal ? newValue: oldValue), self);
+					dirty = true;
+				} else if (self.$$lastDirtyWatch === watcher) {
+					return false;
+				}
+			}
+		} catch(e) {
+			console.error(e);
 		}
 	});
 	return dirty;
@@ -51,7 +65,7 @@ Scope.prototype.$$areEqual = function(newValue, oldValue, valueEq) {
 	if (valueEq) {
 		return _.isEqual(newValue, oldValue);
 	} else {
-		return newValue === oldValue;
+		return newValue === oldValue || (typeof newValue === 'number' && typeof oldValue === 'number' && isNaN(newValue) && isNaN(oldValue));
 	}
 };
 
