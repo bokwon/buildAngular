d8c7961ec70571516aa553faddd9f559fd0001ac
bo kwon <boyoung@whitesky.com>
2017-05-22 16:12:02 -0700

    add scope  method

--- docs/scope.md
+++ docs/scope.md
@@ -7,7 +7,7 @@
 
 ## Methods
 
-### $watch()
+### $watch(watchExpression, listener, [objectEquality])
 
 ```
 $watch(watchExpression, listener, [objectEquality]);
@@ -31,7 +31,7 @@ Registers a listener callback to be executed whenever the watchExpression change
 
 Processes all of the watchers of the current scope and its children. Because a watcher's listener can change the model, the $digest() keeps calling the watchers until no more listeners are firing. This means that it is possible to get into an infinite loop. This function will throw 'Maximum iteration limit exceeded.' if the number of iterations exceeds 10. (TTL: Time To Live. TTL is set to 10)
 
-### $eval()
+### $eval([expression], [locals])
 
 ```
 $eval([expression], [locals]);
@@ -43,3 +43,8 @@ Executes the expression on the current scope and returns the result. Any excepti
 >An AngularJS expression to be executed. (string or function(scope))
 **locals**
 >Local variables object, useful for overriding values in scope.
+
+### $apply([exp])
+
+$apply() is used to execute an expression in AngularJS from outside of the AngularJS framework. (e.g from browser DOM events, setTimeout, XHR or third party libraries)
+The big idea of $apply is that we can execute some code that isn't aware of Angular. This enables to integrate code to the 'Angular lifecycle' using $apply.
--- test/scope_spec.js
+++ test/scope_spec.js
@@ -295,25 +295,47 @@ describe('Scope', function() {
 			expect(scope.counter).toBe(0);
 		});
 	});
-    describe('$eval', function() {
-        var scope;
-        beforeEach(function() {
-            scope = new Scope();
-        });
+	describe('$eval', function() {
+		var scope;
+		beforeEach(function() {
+				scope = new Scope();
+		});
 
-        it('executes $evaled function and returns result', function() {
-            scope.aValue = 42;
-            var result = scope.$eval(function(scope) {
-                return scope.aValue;
-            });
-            expect(result).toBe(42);
-        });
-        it('passes the second $eval argument straight through', function() {
-            scope.aValue = 42;
-            var result = scope.$eval(function(scope, arg) {
-                return scope.aValue + arg;
-            }, 2);
-            expect(result).toBe(44);
-        });
-    });
+		it('executes $evaled function and returns result', function() {
+				scope.aValue = 42;
+				var result = scope.$eval(function(scope) {
+						return scope.aValue;
+				});
+				expect(result).toBe(42);
+		});
+		it('passes the second $eval argument straight through', function() {
+				scope.aValue = 42;
+				var result = scope.$eval(function(scope, arg) {
+						return scope.aValue + arg;
+				}, 2);
+				expect(result).toBe(44);
+		});
+	});
+	describe('$apply', function() {
+		var scope;
+		beforeEach(function() {
+				scope = new Scope();
+		});
+		fit('executes the given function and starts the digest', function() {
+				scope.aValue = 'someValue';
+				scope.counter = 0;
+				scope.$watch(
+						function(scope) { return scope.aValue; },
+						function(newValue, oldValue, scope) {
+								scope.counter++;
+						}
+				);
+				scope.$digest();
+				expect(scope.counter).toBe(1);
+				scope.$apply(function(scope) {
+						scope.aValue = "someOtherValue";
+				});
+				expect(scope.counter).toBe(2);
+		});
+	});
 });
