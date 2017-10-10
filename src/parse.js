a401226e84f722e65f9c80b8f84ffbd75da33a87
Boyoung Kwon <boyoung.kwon@gmail.com>
2017-10-09 20:47:21 -0700

    parsing integers

--- src/parse.js
+++ src/parse.js
@@ -10,17 +10,63 @@ function Lexer() {
 
 Lexer.prototype.lex = function(text) {
 	// Tokenization will be done here.
+	// Token is an object that gives the AST Builder all the information it needs to construct an abstract syntax tree.
+	this.text = text; // the original string
+	this.index = 0;   // our current character index in the string
+	this.ch = undefined; // the curent character
+	this.tokens = [];
+	while(this.index < this.text.length) {
+		this.ch = this.text.charAt(this.index);
+		if (this.isNumber(this.ch)) {
+			this.readNumber();
+		} else {
+			throw 'Unexpected next character: ' + this.ch;
+		}
+	}
+	return this.tokens;
+};
+
+Lexer.prototype.isNumber = function(ch) {
+	return '0' <= ch && ch <= '9';
+};
+
+Lexer.prototype.readNumber = function() {
+	var number = '';
+	while(this.index < this.text.length) {
+		var ch = this.text.charAt(this.index);
+		if (this.isNumber(ch)) {
+			number += ch;
+		} else {
+			break;
+		}
+		this.index++;
+	}
+	this.tokens.push({
+		text: number,
+		value: Number(number)
+	});
 };
 
 //Abstract Syntax Tree (AST) builder
 function AST(lexer) {
 	this.lexer = lexer;
 }
+AST.Program = 'Program';
+AST.Literal = 'Literal';
 
 AST.prototype.ast = function(text) {
 	this.tokens = this.lexer.lex(text);
+	return this.program();
 	// AST building will be done here.
 };
+
+AST.prototype.program = function() {
+	return {type: AST.Program, body: this.constant()};
+};
+
+AST.prototype.constant = function() {
+	return {type: AST.Literal, value: this.tokens[0].value};
+}
 
 function ASTCompiler(astBuilder) {
 	this.astBuilder = astBuilder;
@@ -29,6 +75,20 @@ function ASTCompiler(astBuilder) {
 ASTCompiler.prototype.compile = function(text) {
 	var ast = this.astBuilder.ast(text);
 	//AST compilation will be done here.
+	this.state = {body: []};
+	this.recurse(ast);
+	
+	return new Function(this.state.body.join(''));
+};
+
+ASTCompiler.prototype.recurse = function(ast) {
+	switch(ast.type) {
+		case AST.Program:
+			this.state.body.push('return ', this.recurse(ast.body), ';');
+			break;
+		case AST.Literal:
+			return ast.value;
+	}
 };
 
 function Parser(lexer) {
